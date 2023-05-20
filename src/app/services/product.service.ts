1efc0bdaf4157f49f250e52cdcd7a470dfe95eb0
Myrzatay <86838185+Myrzatay@users.noreply.github.com>
2023-05-21 00:54:04 +0200

    product details

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -13,7 +13,15 @@ export class ProductService {
   private baseUrl = 'http://localhost:8080/api/products';
 
   private categoryUrl = 'http://localhost:8080/api/product-category';
+  
   constructor(private httpClient: HttpClient) { }
+
+  getProduct(theProductId: number): Observable<Product> {
+    
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+    
+    return this.httpClient.get<Product>(productUrl);
+  }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
 
