c9e269b2f24f9f6951a9284d300c4a199f2e92ff
Myrzatay <86838185+Myrzatay@users.noreply.github.com>
2023-05-30 01:00:26 +0200

    login/logout sections

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa9qvhp7354pdXyX5d7',
+        issuer: 'https://dev-86972721.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
